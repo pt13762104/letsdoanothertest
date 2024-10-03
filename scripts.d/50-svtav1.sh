d78cddbce0eb78258bed3d2b8cdeba63a07f0adf
Uranite <62639703+Uranite@users.noreply.github.com>
2024-10-03 21:59:21 +0700

    SVT-AV1-HDR 4.2 "Ghost Robot"

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://github.com/BlueSwordM/svt-av1-psyex.git"
-SCRIPT_COMMIT="fedc23378247b6ab504cbcdfedf428efab9ad452"
+SCRIPT_REPO="https://github.com/juliobbv-p/svt-av1-hdr.git"
+SCRIPT_COMMIT="9dabe3ca04390e2e19267bf87a48c6cab44d6111"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
